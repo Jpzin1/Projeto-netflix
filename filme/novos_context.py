c5d3902f15b7b8fd07b04da31209e291c8cf480e
Jpzin1 <joaojanico@sempreceub.com>
2025-05-22 11:15:26 -0300

    new commit alterando o html da homefilmes e adicionando um carrossel na home

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -1,10 +1,10 @@
 from .models import Filme
 
 def lista_filmes_recentes(request):
-    lista_filmes = Filme.objects.all().order_by('-data_criacao')[:10] # "-" define que está em ordem decrescente
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[:8] # "-" define que está em ordem decrescente
     return {"lista_filmes_recentes": lista_filmes}
 
 def lista_filmes_em_alta(request):
-    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[:10]
+    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[:8]
     return {"lista_filmes_em_alta": lista_filmes}
 
