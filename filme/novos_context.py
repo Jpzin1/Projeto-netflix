72cbe1600b1b55cc3512ba45039755bf27e6bd04
Jpzin1 <joaojanico@sempreceub.com>
2025-05-31 11:48:12 -0300

    atualizando a homefilmes

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -1,10 +1,10 @@
 from .models import Filme
 
 def lista_filmes_recentes(request):
-    lista_filmes = Filme.objects.all().order_by('-data_criacao')[:8] # "-" define que está em ordem decrescente
-    return {"lista_filmes_recentes": lista_filmes}
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:8] # "-" define que está em ordem decrescente
+    filme_destaque = lista_filmes[0]
+    return {"lista_filmes_recentes": lista_filmes, "filme_destaque": filme_destaque}
 
 def lista_filmes_em_alta(request):
-    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[:8]
+    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:8]
     return {"lista_filmes_em_alta": lista_filmes}
-
